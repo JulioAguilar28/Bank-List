1cbf990d0c65b5528f49efacf85d349fe721d981
Julio Aguilar <julio.aguilar9928@gmail.com>
2023-10-04 20:49:11 -0600

    add fonts 🚀

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        Roboto: ['Roboto', 'sans-serif'],
+        'Roboto-Bold': ['Roboto-Bold', 'sans-serif']
+      }
+    }
   },
   plugins: []
 }
